c417a617db1cf2f20e3270b77627c369ae795500
WikkArt <fatimita03@outlook.com>
2024-11-04 00:19:18 -0600

    Pantalla de Opiniones + Diseño de botón de Inicio

--- JS/inicioScript.js
+++ JS/inicioScript.js
@@ -1,26 +1,56 @@
-const bg = document.querySelector('.background-image');
-const windowWidth = window.innerWidth / 5;
-const windowHeight = window.innerHeight / 5;
-let mouseTimeout;
+$(document).ready(function () {
+  // If Else para mantener iniciada o cerrada la sesion del jugador
+  if (localStorage.getItem('loggedIn') === 'true') {
+    $("#btnLogin").hide();
+    $("#btnLogout").show();
+    $("#idImgGoogle").show();
+  } else {
+    $("#btnLogin").show();
+    $("#btnLogout").hide();
+    $("#idImgGoogle").hide();
+  }
 
-bg.addEventListener('mousemove', (e) => {
-  // Clear the timeout to reset the mouse inactivity delay
-  clearTimeout(mouseTimeout);
+  $("#btnLogin").on("click", function () {  
+    $("#btnLogin").hide();
+    $("#btnLogout").show();
+    $("#idImgGoogle").show();
+    localStorage.setItem('loggedIn', 'true');
+  });  
 
-  const mouseX = e.clientX / windowWidth;
-  const mouseY = e.clientY / windowHeight;
+  $("#btnLogout").on("click", function () {  
+    $("#btnLogout").hide();
+    $("#idImgGoogle").hide();
+    $("#btnLogin").show();
+    localStorage.setItem('loggedIn', 'false');
+  });
+});
 
-  // Apply the new transform
-  bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
+
+// Movimiento del fondo con el cursor
+
+// const bg = document.querySelector('.background-image');
+// const windowWidth = window.innerWidth / 5;
+// const windowHeight = window.innerHeight / 5;
+// let mouseTimeout;
+
+// bg.addEventListener('mousemove', (e) => {
+//   // Clear the timeout to reset the mouse inactivity delay
+//   clearTimeout(mouseTimeout);
+
+//   const mouseX = e.clientX / windowWidth;
+//   const mouseY = e.clientY / windowHeight;
+
+//   // Apply the new transform
+//   bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
   
-  // Set a timeout to apply the transition effect after mouse stops moving
-  mouseTimeout = setTimeout(() => {
-    // Apply the transition effect while keeping the current transform
-    bg.style.transition = 'transform 0.15s ease-in-out';
-  }, 300); // A short delay to ensure the transition is applied smoothly
-});
+//   // Set a timeout to apply the transition effect after mouse stops moving
+//   mouseTimeout = setTimeout(() => {
+//     // Apply the transition effect while keeping the current transform
+//     bg.style.transition = 'transform 0.15s ease-in-out';
+//   }, 300); // A short delay to ensure the transition is applied smoothly
+// });
 
-bg.addEventListener('mouseleave', () => {
-  // Apply the transition effect when mouse leaves the background area
-  bg.style.transition = 'transform 0.50s ease-in-out';
-});
+// bg.addEventListener('mouseleave', () => {
+//   // Apply the transition effect when mouse leaves the background area
+//   bg.style.transition = 'transform 0.50s ease-in-out';
+// });
